76cb5e71e69f8225644fc7d810605e342101ec65
JenWeber <weberj10@gmail.com>
2016-04-02 16:05:33 -0400

    connect create organization form to rails db

--- app/organization/route.js
+++ app/organization/route.js
@@ -1,40 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return [
-      {
-      id: 1,
-      name: 'Fall of the Roman Empire',
-      contact: 'Main contact',
-      goal: 'our goal'
-      },
-    ];
-  },
   actions: {
-  createOrganization: function() {console.log('Route Action: Create Organization');},
-}
+      createOrganization: function(properties){
+        console.log('Route Action : createOrganization');
+        this.store.createRecord('organization', properties)
+          .save().then(()=>console.log('record created'));
+      },
+      updateOrganization: function(organization) {
+        console.log('Route Action : updateOrganization');
+        organization.save();
+      },
+      destroyOrganization: function(id){
+        console.log('Route Action : destroyOrganization');
+        this.store.findRecord('organization', id).then((organization) => {
+          this.store.deleteRecord(organization);
+          console.log(`record ${id} destroyed`);
+        });
+      }
+    }
 });
 
 // updateOrganization:  function() {console.log('Route Action: Update Organization');},
 // destroyOrganization:  function() {console.log('Route Action: Destroy Organization');},
-
-
-// actions: {
-//     createOrganization: function(properties){
-//       console.log('Route Action : createOrganization');
-//       this.store.createRecord('organization', properties)
-//         .save().then(()=>console.log('record created'));
-//     },
-//     updateOrganization: function(organization) {
-//       console.log('Route Action : updateOrganization');
-//       organization.save();
-//     },
-//     destroyOrganization: function(id){
-//       console.log('Route Action : destroyOrganization');
-//       this.store.findRecord('organization', id).then((organization) => {
-//         this.store.deleteRecord(organization);
-//         console.log(`record ${id} destroyed`);
-//       });
-//     }
-//   }
