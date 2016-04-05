d196e8363f7c5cf95f389ad91fce467c66fb1b4e
JenWeber <weberj10@gmail.com>
2016-04-05 15:24:44 -0400

    add computed property to org for total activities

--- app/organization/model.js
+++ app/organization/model.js
@@ -1,7 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   contact: DS.attr('string'),
-  goal: DS.attr('string')
+  goal: DS.attr('string'),
+  activities: DS.attr(),
+  activityTotal: Ember.computed('activities',function() {
+    let activities = this.get('activities');
+    return activities.get('length');
+    // return 5;
+  }),
 });
