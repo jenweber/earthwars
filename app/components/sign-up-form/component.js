6a490b856d19c6d8979507540971a4eee039240f
JenWeber <weberj10@gmail.com>
2016-04-05 10:47:44 -0400

    dropdown organizations will submit on signup

--- app/components/sign-up-form/component.js
+++ app/components/sign-up-form/component.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'form',
   classNames: ['form-horizontal'],
+  organizationid: null,
 
   credentials: {},
 
   actions: {
+    selectValue () {
+      // this.set('organizationid', Ember.$('select').val());
+      this.set('credentials.organization_id', Ember.$('select').val());
+    },
     submit () {
       this.sendAction('submit', this.get('credentials'));
     },
--- app/user/model.js
+++ app/user/model.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  organization_id: DS.attr('integer'),
+  organization_id: DS.attr('number'),
   name: DS.attr('string')
 });
