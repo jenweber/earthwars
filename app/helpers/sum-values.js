456040d17b255b3c6f9a591c62e9492b27faf7b6
JenWeber <weberj10@gmail.com>
2016-04-06 16:53:04 -0400

    fix sum helper to handle 0 activities

--- app/helpers/sum-values.js
+++ app/helpers/sum-values.js
@@ -2,8 +2,13 @@ import Ember from 'ember';
 
 export function sumValues(params) {
   // return params[0].map((activity) => Number(activity.get('value'))).reduce((a,b)=> a + b);
-  return 5;
+  if (params[0].map((activity) => Number(activity.get('value'))) === []) {
+    console.log('falsey called');
+    return 0;
+  } else {
+  return params[0].map((activity) => Number(activity.get('value'))).reduce((a,b)=> a + b);
   }
+}
 
 
 export default Ember.Helper.helper(sumValues);
