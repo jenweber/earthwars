33b72dbf1a6b5e59b24e6fba3f92e8722ab804b1
JenWeber <weberj10@gmail.com>
2016-04-07 09:58:58 -0400

    replace router file

---
+++ app/router.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+});
+
+Router.map(function () {
+  this.route('organization');
+  this.route('activities', function() {
+    this.route('activity');
+  });
+  this.route('sign-up');
+  this.route('sign-in');
+  this.route('change-password');
+  this.route('users');
+  this.route('admin-dashboard');
+});
+
+export default Router;
