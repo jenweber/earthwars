29807fa492001a15099573ce2c66fb2fd4bdfeb2
JenWeber <weberj10@gmail.com>
2016-04-06 23:06:30 -0400

    repair error from multiple flash msgs

--- app/sign-in/route.js
+++ app/sign-in/route.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
+  credentials: storageFor('auth'),
+  isAdmin: Ember.computed.bool('credentials.admin'),
 
   actions: {
     signIn (credentials) {
       return this.get('auth').signIn(credentials)
-      .then(() => this.transitionTo('activities'))
+      .then(() => this.get('isAdmin') ? this.transitionTo('admin-dashboard') : this.transitionTo('activities'))
       .then(() => this.get('flashMessages').success('Thanks for signing in!'))
       .catch(() => {
         this.get('flashMessages')
--- app/sign-up/route.js
+++ app/sign-up/route.js
@@ -15,11 +15,11 @@ export default Ember.Route.extend({
       console.log(credentials);
       this.get('auth').signUp(credentials)
       .then(() => this.get('auth').signIn(credentials))
-      .then(() => this.transitionTo('application'))
-      .then(() => {
-        this.get('flashMessages')
-        .success('Successfully signed-up! You have also been signed-in.');
-      })
+      .then(() => this.transitionTo('activities'))
+      // .then(() => {
+      //   this.get('flashMessages')
+      //   .success('Successfully signed-up! You have also been signed-in.');
+      // })
       .catch(() => {
         this.get('flashMessages')
         .danger('There was a problem. Please try again.');
