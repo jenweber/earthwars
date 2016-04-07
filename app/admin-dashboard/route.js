16f00ab2a4efba72226d227510fbd2804fabdaed
JenWeber <weberj10@gmail.com>
2016-04-06 23:32:02 -0400

    create form for updating organization info

--- app/admin-dashboard/route.js
+++ app/admin-dashboard/route.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
 export default Ember.Route.extend({
+  credentials: storageFor('auth'),
   model: function() {
     console.log('organization model called');
     return {
       activities: this.store.findAll('activity'),
+      organization: this.store.find('organization', this.get('credentials.organization_id')),
     };
   }
-
 });
