e85b4d3bd026876ee7bbbe39963764f186e69903
JenWeber <weberj10@gmail.com>
2016-04-26 20:31:25 -0400

    restyle homepage and logins

--- app/sign-in/route.js
+++ app/sign-in/route.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend({
     signIn (credentials) {
       return this.get('auth').signIn(credentials)
       .then(() => this.get('isAdmin') ? this.transitionTo('admin-dashboard') : this.transitionTo('activities'))
-      .then(() => this.get('flashMessages').success('Thanks for signing in!'))
+      // .then(() => this.get('flashMessages').success('Thanks for signing in!'))
       .catch(() => {
         this.get('flashMessages')
         .danger('There was a problem. Please try again.');
